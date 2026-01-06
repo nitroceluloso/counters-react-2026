0cff97906f78e5df772c5cf45bfa8c0eb835e36a
Nitroceluloso <nitroceluloso@mail.me>
2026-01-06 18:01:39 -0300

    feat: add example modal UI.

---
+++ src/modules/counter/components/example-modal/examples.constants.ts
@@ -0,0 +1,7 @@
+export const DRINKS_EXAMPLE = [
+  "cups of coffee",
+  "glasses of water",
+  "Martinis",
+];
+export const FOOD_EXAMPLE = ["Hot-dogs", "Cupcakes eaten", "Chicken wings"];
+export const MISC_EXAMPLE = ["Times sneezed", "Naps", "Day dreaming"];
